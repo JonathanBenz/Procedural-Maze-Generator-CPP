beb6dd8bfa548f4d746c13c42318b1df75a000d8
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2026-05-23 03:26:47 -0400

    OpenGL Rendering Backend

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
+#define _CRTDBG_MAP_ALLOC  // For dumping memory leaks
+#include <stdlib.h>        // For dumping memory leaks
+#include <crtdbg.h>        // For dumping memory leaks
+#include "utils.h"
 #include "grid/public/grid.h"
 #include "algorithms/public/algorithmcontext.h"
 
@@ -9,8 +10,13 @@ int main()
 {
     _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
     _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+
+	if (Utils::InitOpenGL() == -1) return -1;
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_FRAMEBUFFER_SRGB);
+
     /* ========================================================= */
-    // Insert Main Code Here
+    /* Insert Main Code Here */
     {
         Grid grid(5, 5);
 
@@ -19,8 +25,11 @@ int main()
         RandomMazeGenerator.ExecuteOn(grid);
 
         std::cout << grid << std::endl;
+
+        Utils::UpdateLoop();
     }
-    // End Of Program
+    /* End Of Program */
     /* ========================================================= */
+    glfwTerminate();
     _CrtDumpMemoryLeaks();
 }
